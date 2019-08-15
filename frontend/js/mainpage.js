3090473cde96d6ff7b634c84b4fbb7a6582f302d
김강민 <kangminals5610@gmail.com>
2019-08-15 20:36:28 +0900

    [refactor]  uncomment loadDateVideoCards

--- frontend/js/mainpage.js
+++ frontend/js/mainpage.js
@@ -1,8 +1,7 @@
 loadDateVideoCards = function () {
-    // api.get("/api/v1/videos?filter=date&page=0&limit=6")
-    // .then(response => response.json())
-    // .then(json => addVideoCardTemplates(json,"dateVideoCard") )
-    addVideoCardTemplates(data,"dateVideoCard")
+    api.get("/api/v1/videos?filter=date&page=0&limit=6")
+    .then(response => response.json())
+    .then(json => addVideoCardTemplates(json,"dateVideoCard") )
     }
     loadDateVideoCards()
 
