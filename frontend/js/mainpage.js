5a12b105a186b8a61730f5194d804dddcd439c1b
김강민 <kangminals5610@gmail.com>
2019-08-15 16:06:07 +0900

    [refactor] detach header from index.html , modify videocardtemplate.js, mainpge.js

--- frontend/js/mainpage.js
+++ frontend/js/mainpage.js
@@ -1,5 +1,5 @@
 loadDateVideoCards = function () {
-        api.get("/api/v1/videos?filter=view&page=1&limit=6")
+        api.get("/api/v1/videos?filter=date&page=0&limit=6")
         .then(response => response.json())
         .then(json => addVideoCardTemplates(json,"dateVideoCard") )
     }
