d681576dfab70091b727d2ee30f5d7f0507498a4
김강민 <kangminals5610@gmail.com>
2019-08-20 22:07:07 +0900

    [refactor] code formatting

--- backend/src/test/java/com/woowacourse/edd/presentation/controller/UserControllerTests.java
+++ backend/src/test/java/com/woowacourse/edd/presentation/controller/UserControllerTests.java
@@ -18,24 +18,24 @@ public class UserControllerTests extends EddApplicationTests {
     void user_save() {
         UserRequestDto userSaveRequestDto = new UserRequestDto("robby", "shit@email.com", "P@ssW0rd");
         EntityExchangeResult<byte[]> result = webTestClient.post()
-                .uri(USER_URL)
-                .body(Mono.just(userSaveRequestDto), UserRequestDto.class)
-                .exchange()
-                .expectStatus()
-                .isCreated()
-                .expectHeader().valueMatches("Location", USER_URL + "/\\d")
-                .expectBody()
-                .returnResult();
+            .uri(USER_URL)
+            .body(Mono.just(userSaveRequestDto), UserRequestDto.class)
+            .exchange()
+            .expectStatus()
+            .isCreated()
+            .expectHeader().valueMatches("Location", USER_URL + "/\\d")
+            .expectBody()
+            .returnResult();
 
         webTestClient
-                .get()
-                .uri(result.getResponseHeaders().getLocation().toASCIIString())
-                .exchange()
-                .expectStatus()
-                .isOk()
-                .expectBody()
-                .jsonPath("$.name").isEqualTo("robby")
-                .jsonPath("$.email").isEqualTo("shit@email.com");
+            .get()
+            .uri(result.getResponseHeaders().getLocation().toASCIIString())
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectBody()
+            .jsonPath("$.name").isEqualTo("robby")
+            .jsonPath("$.email").isEqualTo("shit@email.com");
     }
 
     @Test
@@ -43,32 +43,32 @@ public class UserControllerTests extends EddApplicationTests {
         UserRequestDto userRequestDto = new UserRequestDto("jm", "hansome@gmail.com", "P!ssW0rd");
 
         webTestClient.put()
-                .uri(USER_URL + "/1")
-                .body(Mono.just(userRequestDto), UserRequestDto.class)
-                .exchange()
-                .expectStatus()
-                .isOk()
-                .expectBody()
-                .jsonPath("$.name").isEqualTo(userRequestDto.getName())
-                .jsonPath("$.email").isEqualTo(userRequestDto.getEmail());
+            .uri(USER_URL + "/1")
+            .body(Mono.just(userRequestDto), UserRequestDto.class)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectBody()
+            .jsonPath("$.name").isEqualTo(userRequestDto.getName())
+            .jsonPath("$.email").isEqualTo(userRequestDto.getEmail());
     }
 
     @Test
     void user_delete_not_found() {
         webTestClient.delete()
-                .uri(USER_URL + "/999")
-                .exchange()
-                .expectStatus()
-                .isNotFound();  //404
+            .uri(USER_URL + "/999")
+            .exchange()
+            .expectStatus()
+            .isNotFound();  //404
     }
 
     @Test
     void user_delete_no_content() {
         webTestClient.delete()
-                .uri(USER_URL + "/1")
-                .exchange()
-                .expectStatus()
-                .isNoContent();
+            .uri(USER_URL + "/1")
+            .exchange()
+            .expectStatus()
+            .isNoContent();
     }
 
     @Test
@@ -76,27 +76,27 @@ public class UserControllerTests extends EddApplicationTests {
         UserRequestDto userRequestDto = new UserRequestDto("conas", "conas@email.com", "P@ssW0rd");
 
         EntityExchangeResult<byte[]> result = webTestClient
-                .post()
-                .uri(USER_URL)
-                .body(Mono.just(userRequestDto), UserRequestDto.class)
-                .exchange()
-                .expectStatus()
-                .isCreated()
-                .expectHeader().valueMatches("Location", USER_URL + "/\\d")
-                .expectBody()
-                .returnResult();
+            .post()
+            .uri(USER_URL)
+            .body(Mono.just(userRequestDto), UserRequestDto.class)
+            .exchange()
+            .expectStatus()
+            .isCreated()
+            .expectHeader().valueMatches("Location", USER_URL + "/\\d")
+            .expectBody()
+            .returnResult();
 
         String url = result.getResponseHeaders().getLocation().toASCIIString();
         webTestClient.delete()
-                .uri(url)
-                .exchange()
-                .expectStatus()
-                .isNoContent();
+            .uri(url)
+            .exchange()
+            .expectStatus()
+            .isNoContent();
 
         webTestClient.delete()
-                .uri(url)
-                .exchange()
-                .expectStatus()
-                .isNotFound();
+            .uri(url)
+            .exchange()
+            .expectStatus()
+            .isNotFound();
     }
 }
