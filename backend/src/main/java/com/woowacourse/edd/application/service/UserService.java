a74ca4bf265efeba8c7a57e5d48b5d4c955ca3c7
김강민 <kangminals5610@gmail.com>
2019-08-22 16:04:21 +0900

    [refactor] change variable name in userService

--- backend/src/main/java/com/woowacourse/edd/application/service/UserService.java
+++ backend/src/main/java/com/woowacourse/edd/application/service/UserService.java
@@ -21,13 +21,13 @@ public class UserService {
         return user.getId();
     }
 
-    public UserResponse update(Long id, Long sessionId, UserRequestDto userRequestDto) {
-        User user = userInternalService.update(id, sessionId, userRequestDto);
+    public UserResponse update(Long id, Long loggedInId, UserRequestDto userRequestDto) {
+        User user = userInternalService.update(id, loggedInId, userRequestDto);
         return userConverter.toResponse(user);
     }
 
-    public void delete(Long id, Long sessionId) {
-        userInternalService.delete(id, sessionId);
+    public void delete(Long id, Long loggedInId) {
+        userInternalService.delete(id, loggedInId);
     }
 
     public UserResponse findbyId(Long id) {
