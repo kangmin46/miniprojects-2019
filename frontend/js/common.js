3331e3b765174a793a943073078861394b548ece
김강민 <kangminals5610@gmail.com>
2019-08-27 01:28:57 +0900

    [feat] add requestMyChannelVideos api in common.js,

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -1,9 +1,13 @@
 const wootubeCtx = {
     util: {
-        getUrlParams: function () {
-            const params = {};
-            window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (str, key, value) { params[key] = value; });
-            return params;
+        getUrlParams: function (name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, '\\$&');
+            var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, ' '));
         },
         calculateDate: function (responseDate) {
             const localResponseDate = moment.utc(responseDate,'YYYYMMDDHH').local().format('YYYYMMDDHH')
@@ -38,7 +42,23 @@ const Api = function () {
         'Content-Type': 'application/json'
     }
 
-    const baseUrl = '/api'
+    const baseUrl = 'http://localhost:8080'
+
+    const requestUser = (id) => {
+        return request(`${baseUrl}/v1/users/${id}`, 'GET');
+    }
+
+    const updateUser = (id, body) => {
+        return request(`${baseUrl}/v1/users/${id}`, 'PUT', body)
+    }
+
+    const deleteUser = (id) => {
+        return requestWithoutBody(`${baseUrl}/v1/users/${id}`, 'DELETE')
+    }
+
+    const retrieveLoginInfo = () => {
+        return requestWithoutBody(`${baseUrl}/v1/login/users`, 'GET')
+    }
 
     const request = (url, method, body) => {
         return fetch(url, {
@@ -59,6 +79,10 @@ const Api = function () {
     
     const requestVideos = (page, size, sort) => {
         return requestWithoutBody(`${baseUrl}/v1/videos?page=${page}&size=${size}&sort=${sort},DESC`,'GET')
+    }
+
+    const requestMyChannelVideos = (userId) => {
+        return requestWithoutBody(`${baseUrl}/v1/users/${userId}/videos`,'GET')
     }
 
     const requestVideo = (videoId) => {
@@ -92,7 +116,12 @@ const Api = function () {
         updateVideo,
         deleteVideo,
         postLogin,
-        signup
+        signup,
+        requestUser,
+        updateUser,
+        retrieveLoginInfo,
+        deleteUser,
+        requestMyChannelVideos
     }
 
 }
